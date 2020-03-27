b797dd7878ff35d72e165711ac2e6eda84facece
rintaro9147 <rintaroyoshikawa1204@gmail.com>
2020-03-27 21:04:17 +0900

    make it better

--- index.php
+++ index.php
@@ -1,22 +1,15 @@
 <?php
-    // Composerでインストールしたライブラリを一括読み込み
-    require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
-    // アクセストークンを使いCurlHTTPClientをインスタンス化
-    $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('Channel Access Token');
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
+$sign = $_SERVER["HTTP_" . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
+$events = $bot->parseEventRequest(file_get_contents('php://input'), $sign);
 
-    //CurlHTTPClientとシークレットを使いLINEBotをインスタンス化
-    $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => 'Channel Secret']);
-
-    // LINE Messaging APIがリクエストに付与した署名を取得
-    $signature = $_SERVER["HTTP_" . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
-
-    //署名をチェックし、正当であればリクエストをパースし配列へ、不正であれば例外処理
-    $events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
-
-    foreach ($events as $event) {
-        // メッセージを返信
-        $response = $bot->replyMessage(
-            $event->getReplyToken(), new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($event->getText())  
-        );
-    }
+foreach ($events as $event) {
+	if (!($event instanceof \LINE\LINEBot\Event\MessageEvent) ||
+	    !($event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage)) {
+	    continue;
+	}
+	$bot->replyText($event->getReplyToken(), $event->getText());
+}
